683e2851cd6d190cc2d620c8e119fbc65f0a18bd
Aiswarya S <aaiswaryas004@gmail.com>
2026-09-25 16:41:01 +0530

    Update backend for production deployment

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://trial-virtual-tryon.netlify.app",
     credentials: true
 }));
 app.use(cookieParser());
